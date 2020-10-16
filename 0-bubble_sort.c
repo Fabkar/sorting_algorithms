fd6327fb11f495745d0bb49a636069c69c47adcc
luiscarvil <1888@holbertonschool.com>
2020-10-16 14:32:50 -0500

    add actualization bubble sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,11 +14,11 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < (size - i); j++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[j - 1] > array[j])
 			{
 				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				array[j] = array[j - 1];
+				array[j - 1] = tmp;
 				print_array(array, size);
 			}
 		}
